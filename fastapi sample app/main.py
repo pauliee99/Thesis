21559e6d1f630fa86ef66296e99796ea5fde2cb7
pauliee99 <pavlosan67@gmail.com>
2023-12-03 20:05:21 +0200

    delete extra files

--- fastapi sample app/main.py
+++ fastapi sample app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Response
+from pydantic import BaseModel, StrictBool
+from typing import List
 
 app = FastAPI()
 
@@ -11,6 +12,28 @@ class User(BaseModel):
 
 user1 = User(id=1, name='pavlos', surname='andreou', email='pavlos@email.com')
 
+users = []
+users.append(user1)
+
 @app.get("/")
 def index():
     return user1
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int) -> List[User]:
+    result = list(filter(lambda x: x.id == user_id, users))
+    return result
+
+@app.post("/user")
+def create_user(user: User) -> User:
+    users.append(user)
+    return user
+
+@app.delete("/user/{user_id}")
+def remove_user(user_id: int, response: Response) -> StrictBool:
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return True
+    response.status_code=404
+    return False
