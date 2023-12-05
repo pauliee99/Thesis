62933df0c9b4f953c8805f102b0b700410b11864
pauliee99 <pavlosan67@gmail.com>
2023-12-05 02:07:46 +0200

    events created

--- fastapi sample app/main.py
+++ fastapi sample app/main.py
@@ -1,42 +1,56 @@
+from datetime import datetime
 from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 from enum import Enum
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+class Student(BaseModel):
+    id: int
+    fname: str
+    lname: str
+    username: str
+    password: str
+    student_id: int
+    profile_picture: str
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+class Manager(BaseModel):
+    id: int
+    fname: str
+    lname: str
+    username: str
+    password: str
+    profile_picture: str
+
+class Admin(BaseModel):
+    id: int
+    fname: str
+    lname: str
+    username: str
+    password: str
+    profile_picture: str
+
+class Event(BaseModel):
+    id: int
+    displayname: str
+    location: str
+    start_time: datetime
+    end_time: datetime
+    price: float
+    picture: str
+    description: str
+
+events = []
+event2 = Event(id=2, displayname="event2", location="nea smirni", start_time=datetime.now(), end_time=datetime.now(), price=1.0, picture="picture here", description="another cool event")
+events.append(Event(id=1, displayname="event1", location="kallithea", start_time=datetime.now(), end_time=datetime.now(), price=0.0, picture="picture here", description="a very cool event"))
+events.append(event2)
 
 app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/events/")
+def read_item():
+    return events
