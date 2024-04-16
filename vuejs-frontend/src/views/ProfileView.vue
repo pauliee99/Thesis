e3d5a93b99d2dac248a7fa046bf33f5973e78b2d
pauliee99 <pavlosan67@gmail.com>
2024-04-17 01:29:47 +0300

    starting fixing profile page adding minio links

--- vuejs-frontend/src/views/ProfileView.vue
+++ vuejs-frontend/src/views/ProfileView.vue
@@ -12,6 +12,11 @@ const { userData } = useApplicationStore();
                         <h1 class="fs-3">Profile</h1>
                     </div>
                     <div>
+                        <div class="profileviewcircle">
+                            <img v-if="userData.picture === undefined" src="http://192.168.2.9:9001/api/v1/buckets/profile-pictures/objects/download?preview=true&prefix=cHJvZmlsZS1kZWZhdWx0LnBuZw==&version_id=null" alt="Profile Picture" class="profile-img">
+                            <img v-else :src="userData.profile_picture" alt="Profile Picture" class="profile-img">
+                            <img id="edit-profilepicture" class="profile-img" src="http://192.168.2.9:9001/api/v1/buckets/icons/objects/download?preview=true&prefix=ZWRpdC1wcm9maWxlLXBpY3R1cmUuc3Zn&version_id=null"></img>
+                        </div>
                         <!-- @EXERCISE: Create a nice component to present user data -->
                         <pre>{{ userData }}</pre>
                     </div>
@@ -20,3 +25,4 @@ const { userData } = useApplicationStore();
         </div>
     </div>
 </template>
+<style src="../assets/profile.css"></style>
